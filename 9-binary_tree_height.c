5372ff41c2d1faa22c14d11737d606a7374b2082
CObitimah <obitimahcephas@gmail.com>
2024-04-13 19:08:52 +0000

    a task

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,9 +1,8 @@
-
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - checks if a node is a leaf
- * @node: pointer to the node to check
+ * binary_tree_is_leaf - Checks if a node is a leaf
+ * @node: Pointer to the node to check
  *
  * Return: 1 if node is a leaf, and 0 otherwise. If node is NULL, return 0
  */
@@ -15,20 +14,22 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 }
 
 /**
- * binary_tree_height - measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height of
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height of
  *
- * Return: the height of the tree. If tree is NULL, return 0
+ * Return: The height of the tree. If tree is NULL, return 0
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left, right;
+	size_t left_height, right_height;
 
 	if (tree == NULL || binary_tree_is_leaf(tree))
 		return (0);
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
-	if (left >= right)
-		return (1 + left);
-	return (1 + right);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height >= right_height)
+		return (1 + left_height);
+	return (1 + right_height);
 }
