922bb4542462e4d839198efb03c56f9be7db3a49
CObitimah <obitimahcephas@gmail.com>
2024-04-16 17:47:02 +0000

    AVL Insert

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,9 +1,6 @@
-#include "binary_trees.h"
-
-size_t height(const binary_tree_t *tr);
 int balance(const binary_tree_t *tr);
 avl_t *avl_insert_recursive(avl_t **tr, avl_t *p,
-		avl_t **new, int val);
+                avl_t **new, int val);
 avl_t *avl_insert(avl_t **tr, int val);
 
 /**
@@ -14,15 +11,15 @@ avl_t *avl_insert(avl_t **tr, int val);
  */
 size_t height(const binary_tree_t *tr)
 {
-	if (tr != NULL)
-	{
-		size_t l = 0, r = 0;
+        if (tr != NULL)
+        {
+                size_t l = 0, r = 0;
 
-		l = tr->left ? 1 + height(tr->left) : 1;
-		r = tr->right ? 1 + height(tr->right) : 1;
-		return ((l > r) ? l : r);
-	}
-	return (0);
+                l = tr->left ? 1 + height(tr->left) : 1;
+                r = tr->right ? 1 + height(tr->right) : 1;
+                return ((l > r) ? l : r);
+        }
+        return (0);
 }
 
 /**
@@ -33,7 +30,7 @@ size_t height(const binary_tree_t *tr)
  */
 int balance(const binary_tree_t *tr)
 {
-	return (tr != NULL ? height(tr->left) - height(tr->right) : 0);
+        return (tr != NULL ? height(tr->left) - height(tr->right) : 0);
 }
 
 /**
@@ -46,66 +43,6 @@ int balance(const binary_tree_t *tr)
  * Return: A pointer to the new root after insertion, or NULL on failure.
  */
 avl_t *avl_insert_recursive(avl_t **tr, avl_t *p,
-		avl_t **new, int val)
+                avl_t **new, int val)
 {
-	int bfactor;
-
-	if (*tr == NULL)
-		return (*new = binary_tree_node(p, val));
-
-	if ((*tr)->n > val)
-	{
-		(*tr)->left = avl_insert_recursive(&(*tr)->left, *tr, new, val);
-		if ((*tr)->left == NULL)
-			return (NULL);
-	}
-	else if ((*tr)->n < val)
-	{
-		(*tr)->right = avl_insert_recursive(&(*tr)->right, *tr, new, val);
-		if ((*tr)->right == NULL)
-			return (NULL);
-	}
-	else
-		return (*tr);
-
-	bfactor = balance(*tr);
-	if (bfactor > 1 && (*tr)->left->n > val)
-		*tr = binary_tree_rotate_right(*tr);
-	else if (bfactor < -1 && (*tr)->right->n < val)
-		*tr = binary_tree_rotate_left(*tr);
-	else if (bfactor > 1 && (*tr)->left->n < val)
-	{
-		(*tr)->left = binary_tree_rotate_left((*tr)->left);
-		*tr = binary_tree_rotate_right(*tr);
-	}
-	else if (bfactor < -1 && (*tr)->right->n > val)
-	{
-		(*tr)->right = binary_tree_rotate_right((*tr)->right);
-		*tr = binary_tree_rotate_left(*tr);
-	}
-
-	return (*tr);
-}
-
-/**
- * avl_insert - Inserts a value into an AVL tree.
- * @tr: A double pointer to the root node of the AVL tree to insert into.
- * @val: The value to insert into the AVL tree.
- *
- * Return: A pointer to the inserted node, or NULL on failure.
- */
-avl_t *avl_insert(avl_t **tr, int val)
-{
-	avl_t *new = NULL;
-
-	if (tr == NULL)
-		return (NULL);
-	if (*tr == NULL)
-	{
-		*tr = binary_tree_node(NULL, val);
-		return (*tr);
-	}
-	avl_insert_recursive(tr, *tr, &new, val);
-	return (new);
-}
-
+                                                                2,1        
